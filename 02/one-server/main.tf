2f77406f41933247e024710bd53eb9b976b0e476
ktk3724 <ktk3724@naver.com>
2025-01-09 22:23:14 +0900

    6th commit

---
+++ 02/one-server/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  # Configuration options
+    region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0b4624933067d393a"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "terraform-example"
+    }
+}
